53563ce086f2caa7bc77ba3eb113a36de125fdf9
Zarifzz <mylegendaries75@gmail.com>
2023-08-03 14:17:33 -0700

    finished home request methods

--- main.py
+++ main.py
@@ -7,11 +7,48 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "supderdupersecretkey"
 socketio = SocketIO(app)
 
+rooms = {}
+
+def generate_unique_code(len):
+    while True
+        code = ''
+        for _ in range(len):
+            code += random.choice(ascii_uppercase)
+
+        if code not in rooms:
+            break
+    return code
+
+
+
 
 
 @app.route("/", methods=["POST", "GET"])
 def home():
-    return render_template("")
+    if request.method == "POST":
+        name = request.form.get("name")
+        code = request.form.get("code")
+        join = request.form.get("join", False)
+        create = request.form.get("create", False)
+
+        if not name:
+            return render_template("home.html", error="Enter a Name!", code=code, name=name)
+        
+        if join != False and not code:
+            return render_template("home.html", error="Enter a Code!",  code=code, name=name)
+        
+        room = code
+        if create != False:
+            room = generate_unique_code(4)
+            rooms[room] = {"members": 0, "messages": [] }
+        elif code not in rooms:
+            return render_template("home.html",  error="Room does not exist",  code=code, name=name)
+
+        
+
+
+
+    return render_template("home.html")
 
 
 if __name__ == "__main__":
