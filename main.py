d895e8ec2a66f1136551ed3351630746cde17794
Zarifzz <mylegendaries75@gmail.com>
2023-08-03 20:27:10 -0700

    start of socketIO

--- main.py
+++ main.py
@@ -58,10 +58,24 @@ def room():
     if room is None or session.get("name") is None or room not in rooms:
         return redirect(url_for("home"))
     
-    
+
 
     return render_template("room.html")
 
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+
+    if not room or not name:
+        return 
+    if room not in rooms:
+        leave_room(room)
+        return 
+    
+    join_room(room)
+    
 
 
 
