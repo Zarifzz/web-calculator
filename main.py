099f0710a55973c16b3f2a92be9a45352280059d
Zarifzz <mylegendaries75@gmail.com>
2023-08-08 17:47:26 -0700

    made simple board

--- main.py
+++ main.py
@@ -55,12 +55,12 @@ def home():
 @app.route("/room")
 def room():
     room = session.get("room")
-    if room is None or session.get("name") is None or room not in rooms:
-        return redirect(url_for("home"))
+    # if room is None or session.get("name") is None or room not in rooms:
+    #     return redirect(url_for("home"))
     
 
 
-    return render_template("room.html", code=room, messages=rooms[room]["messages"])
+    return render_template("room.html") #    , code=room, messages=rooms[room]["messages"])
 
 
 @socketio.on("message")
